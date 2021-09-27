2db9f20d0b6be98a1ac84fcf4b8ae69a5c2133c4
Jullyana Mattos Vycas <jumattos@microsoft.com>
2021-09-27 15:01:13 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace HyperlinkOpTimedOutDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static RoutedUICommand ShowDialogCommand
+            = new RoutedUICommand("Show dialog",
+                                  nameof(ShowDialogCommand),
+                                  typeof(MainWindow));
+
+        private void CanShowDialog(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ShowDialog(object target, ExecutedRoutedEventArgs e)
+        {
+            _ = MessageBox.Show(owner: this,
+                messageBoxText: "Are you sure you want to continue?",
+                caption: "Continue Confirmation",
+                button: MessageBoxButton.YesNo,
+                icon: MessageBoxImage.Information,
+                defaultResult: MessageBoxResult.No);
+        }
+    }
+}
